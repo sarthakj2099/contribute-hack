204fe55c6769e544c67b3a0cda3cfe0efbe1c438
sarthakj2099 <42967784+sarthakj2099@users.noreply.github.com>
2020-07-02 13:35:21 +0530

    hack-commit

---
+++ Z.java
@@ -0,0 +1,23 @@
+import java.util.*;
+          
+          public class Main {
+          
+              public static void main(String[] args){
+             	      //   System.out.println("*****");
+             	      //   System.out.println("   *");
+             	      //   System.out.println("  *");
+             	      //   System.out.println(" *");
+             	      //   System.out.print("*****");
+             	      int n=5;
+             	      for(int i=0;i<n;i++){
+             	          if(i==0||i==n-1)
+             	              for(int j=0;j<n-1;j++)
+             	                System.out.print("*");
+             	          else
+             	              for(int j=0;j<n-1-i;j++)
+             	                System.out.print(" ");
+             	          System.out.println("*");
+             	      }
+         
+              }
+         }0 days ago
